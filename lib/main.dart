d62652dcfadfc16e3487906fa5bc2baa2b955502
mina <nnkm8329@gmail.com>
2025-10-14 03:15:36 +0900

    appbarの基本的なレイアウトを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,8 +18,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        title: const Text('Home Page'),
+        backgroundColor: Colors.white,
+        // 文字とかを黒にする
+        foregroundColor: Colors.black,
+        title: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.amber,
+            radius: 25,
+            child: Image.asset('assets/profile.png'),
+          ),
+          title: Text(
+            'Hello, I\'m',
+            style: TextStyle(fontSize: 12, color: Colors.grey.shade400),
+          ),
+          subtitle: Text(
+            'minato',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.calendar,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.bell,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
